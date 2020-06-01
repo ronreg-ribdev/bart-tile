3ade81e6667fcbc0424022c6d8522c1d63a47599
ronreg-ribdev <56719257+ronreg-ribdev@users.noreply.github.com>
2020-06-01 02:39:14 -0700

    Start adding ElevatorWidget

--- src/js/components/root.js
+++ src/js/components/root.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import _ from 'lodash';
 import { HeaderBar } from "./lib/header-bar.js"
+
+class ElevatorWidget extends Component {
+  render() {
+    return (<div>
+      Elevator info
+    </div>);
+  }
+}
 
 class TimeScheduleWidget extends Component {
   constructor(props) {
@@ -84,10 +92,12 @@ export class Root extends Component {
       <BrowserRouter>
         <div className="absolute h-100 w-100 bg-gray0-d ph4-m ph4-l ph4-xl pb4-m pb4-l pb4-xl">
         <HeaderBar/>
+        <Route exact path="/~bartinfo/elevators" render={ () => <ElevatorWidget/> }/>
         <Route exact path="/~bartinfo" render={ () => {
           return (
             <div className="cf w-100 flex flex-column pa4 ba-m ba-l ba-xl b--gray2 br1 h-100 h-100-minus-40-s h-100-minus-40-m h-100-minus-40-l h-100-minus-40-xl f9 white-d overflow-x-hidden">
               <h1 className="mt0 f8 fw4">BART Info</h1>
+              <Link to="/~bartinfo/elevators">Elevator information</Link>
               <div style={{display: "grid", gridTemplateColumns: "66% 34%", gridGap: "10px" }}>
                 <div className="topinfo" style={{gridColumn: "1 / 2"}}>
                   <TimeScheduleWidget/>
